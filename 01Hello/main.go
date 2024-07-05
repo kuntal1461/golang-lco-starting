e5a76f27afdf90cfeb080b586a63d6c2a5c45f99
kuntal.1461 <kuntal.1461@gmail.com>
2024-07-05 22:16:59 +0530

    first commit

---
+++ 01Hello/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Kuntal , This is go language.")
+}
---
+++ 02variable/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+const LongId= 3000
+
+func main()  {
+	var userName string = "Kuntal"
+	fmt.Println(userName)
+	fmt.Printf("The varibale type is : %T ", userName)
+
+
+	fmt.Println(LongId);
+
+
+	userCode :=400
+	fmt.Println("the total usrcode is ",userCode);
+
+
+
+}
